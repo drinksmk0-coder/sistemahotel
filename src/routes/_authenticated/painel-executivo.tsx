7139ea1680666cebbb702f540ca8086530c5767b
drinksmk0-coder <drinksmk0@gmail.com>
2026-07-31 16:48:07 -0300

    aplicar correções visuais do painel executivo

--- src/routes/_authenticated/painel-executivo.tsx
+++ src/routes/_authenticated/painel-executivo.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { ExecutiveBiDashboard } from "@/components/executive/ExecutiveBiDashboard";
+import "@/components/executive/executive-dashboard-fixes.css";
 
 export const Route = createFileRoute("/_authenticated/painel-executivo")({
   component: ExecutiveBiDashboard,
