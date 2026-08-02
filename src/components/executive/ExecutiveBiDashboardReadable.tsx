f4d0a880800d490afa294abffd03189e6c44ae83
drinksmk0-coder <drinksmk0@gmail.com>
2026-08-02 18:51:56 -0300

    Aplicar grade compacta e card de descrições das despesas

--- src/components/executive/ExecutiveBiDashboardReadable.tsx
+++ src/components/executive/ExecutiveBiDashboardReadable.tsx
@@ -1,14 +1,17 @@
 import { ExecutiveCancellationImpact } from "@/components/executive/ExecutiveCancellationImpact";
 import { ExecutiveDashboardInteractions } from "@/components/executive/ExecutiveDashboardInteractions";
+import { ExecutiveDashboardLayoutOrganizer } from "@/components/executive/ExecutiveDashboardLayoutOrganizer";
 import { ExecutiveDashboardReference } from "@/components/executive/ExecutiveDashboardReference";
 import { ExecutiveDashboardUiGuard } from "@/components/executive/ExecutiveDashboardUiGuard";
 import { ExecutiveDonutLegendStandardizer } from "@/components/executive/ExecutiveDonutLegendStandardizer";
 import { ExecutiveExpenseCategoryFrequency } from "@/components/executive/ExecutiveExpenseCategoryFrequency";
+import { ExecutiveExpenseDescriptions } from "@/components/executive/ExecutiveExpenseDescriptions";
 import { ExecutiveRevenueExpenseGopChart } from "@/components/executive/ExecutiveRevenueExpenseGopChart";
 import "@/components/executive/executive-dashboard-reference-enhancements.css";
 import "@/components/executive/executive-dashboard-responsive.css";
 import "@/components/executive/executive-dashboard-financial-map.css";
 import "@/components/executive/executive-dashboard-donut-legend.css";
+import "@/components/executive/executive-dashboard-compact-layout.css";
 
 export function ExecutiveBiDashboardReadable() {
   return (
@@ -21,7 +24,9 @@ export function ExecutiveBiDashboardReadable() {
       <ExecutiveRevenueExpenseGopChart />
       <ExecutiveCancellationImpact />
       <ExecutiveExpenseCategoryFrequency />
+      <ExecutiveExpenseDescriptions />
       <ExecutiveDonutLegendStandardizer />
+      <ExecutiveDashboardLayoutOrganizer />
       <ExecutiveDashboardUiGuard />
     </div>
   );
