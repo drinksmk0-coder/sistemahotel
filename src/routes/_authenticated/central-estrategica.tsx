78639d4c0e8433a97d3696d87ab357eed694c94c
drinksmk0-coder <drinksmk0@gmail.com>
2026-08-10 18:07:32 -0300

    fix: torna painel atraente a tela principal da V2

--- src/routes/_authenticated/central-estrategica.tsx
+++ src/routes/_authenticated/central-estrategica.tsx
@@ -5,5 +5,5 @@ export const Route = createFileRoute("/_authenticated/central-estrategica")({
 });
 
 function CentralEstrategicaRedirect() {
-  return <Navigate to="/painel-executivo" replace />;
+  return <Navigate to="/painel-atraente" replace />;
 }
