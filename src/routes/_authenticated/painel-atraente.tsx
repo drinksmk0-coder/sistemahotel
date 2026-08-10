8cdfe62d1c4eb22efdb726cf8102c7acd6974f3e
drinksmk0-coder <drinksmk0@gmail.com>
2026-08-10 19:26:01 -0300

    fix: carrega legenda visível no BI V4

--- src/routes/_authenticated/painel-atraente.tsx
+++ src/routes/_authenticated/painel-atraente.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { PainelAtraenteDashboardV4 } from "@/components/executive/PainelAtraenteDashboardV4";
 import "./painel-atraente-v2.css";
+import "./painel-atraente-v4-legend.css";
 
 export const Route = createFileRoute("/_authenticated/painel-atraente")({
   component: PainelAtraente,
