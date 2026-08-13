4dc39cfe54351a84df153d5c4afec5484d175fd3
drinksmk0-coder <drinksmk0@gmail.com>
2026-08-13 16:17:49 -0300

    refactor: ativa painel com filtros propagados

--- src/routes/_authenticated/painel-executivo.tsx
+++ src/routes/_authenticated/painel-executivo.tsx
@@ -1,12 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { UnifiedHotelExecutiveDashboard } from "@/components/executive/UnifiedHotelExecutiveDashboard";
+import { FilteredHotelExecutiveDashboard } from "@/components/executive/FilteredHotelExecutiveDashboard";
 
 export const Route = createFileRoute("/_authenticated/painel-executivo")({ component: PainelExecutivoRoute });
 
 function PainelExecutivoRoute() {
   return (
     <div className="space-y-3">
-      <UnifiedHotelExecutiveDashboard />
+      <FilteredHotelExecutiveDashboard />
     </div>
   );
 }
