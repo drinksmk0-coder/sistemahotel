a5173b50bbf5ddf2056ec0272257312351fc7983
drinksmk0-coder <drinksmk0@gmail.com>
2026-08-14 20:50:53 -0300

    preview: torna tela de reservas adaptável a diferentes larguras

--- src/routes/_authenticated/reservas.tsx
+++ src/routes/_authenticated/reservas.tsx
@@ -1,6 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { ReservasModernV2 } from "@/components/ReservasModernV2";
+import "./reservas-responsive-v2.css";
 
 export const Route = createFileRoute("/_authenticated/reservas")({
-  component: ReservasModernV2,
+  component: ReservasResponsiveRoute,
 });
+
+function ReservasResponsiveRoute() {
+  return (
+    <div className="reservas-responsive-shell">
+      <ReservasModernV2 />
+    </div>
+  );
+}
