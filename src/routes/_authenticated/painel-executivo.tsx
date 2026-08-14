5034666df4ba3219d3e1a2ae648fb0e200cc6d4b
drinksmk0-coder <drinksmk0@gmail.com>
2026-08-14 19:10:09 -0300

    preview: aponta painel executivo para nova versão

--- src/routes/_authenticated/painel-executivo.tsx
+++ src/routes/_authenticated/painel-executivo.tsx
@@ -1,12 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { FilteredHotelExecutiveDashboard } from "@/components/executive/FilteredHotelExecutiveDashboard";
+import { ExecutiveDecisionDashboard } from "@/components/executive/ExecutiveDecisionDashboard";
 
 export const Route = createFileRoute("/_authenticated/painel-executivo")({ component: PainelExecutivoRoute });
 
 function PainelExecutivoRoute() {
   return (
     <div className="space-y-3">
-      <FilteredHotelExecutiveDashboard />
+      <ExecutiveDecisionDashboard />
     </div>
   );
 }
